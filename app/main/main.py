c12176935e0bf27e2ed0dfb3fddde8c1aee2c599
Nico Braun <nico-braun@live.de>
2021-04-25 14:58:13 +0000

    add error handling

--- app/main/main.py
+++ app/main/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify, request, Response, current_app as app
+from flask import Blueprint, jsonify, request, Response, abort, current_app as app
 from bson.json_util import dumps
+from pymongo import errors
 
 
 def create(client):
@@ -8,14 +9,31 @@ def create(client):
 
     @main.route("/")
     def index():
-        msgs = messages.find()
+        try:
+            msgs = messages.find()
+        except errors.ServerSelectionTimeoutError as timeout:
+            app.log_exception(timeout)
+            abort(503)
+        except Exception as generic:
+            app.log_exception(generic)
+            abort(500)
+
         return Response(dumps(msgs), content_type="application/json")
 
     @main.route("/msg", methods=["POST"])
     def msg():
+        if not request.data:
+            abort(400)
         content = request.get_json(silent=True)
-        app.logger.info(f"message posted: {content}")
-        result = messages.insert_one({"message": content.get("message")})
+        app.logger.debug(f"message posted: {content}")
+        try:
+            result = messages.insert_one({"message": content.get("message")})
+        except errors.ServerSelectionTimeoutError as timeout:
+            app.log_exception(timeout)
+            abort(503)
+        except Exception as generic:
+            app.log_exception(generic)
+            abort(400)
         payload = jsonify(id=str(result.inserted_id))
         app.logger.debug(f"payload: {payload}")
         return payload, 201
