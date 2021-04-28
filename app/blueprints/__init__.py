3dfd3db7f05e0787fb1a2dda8d7e1a081db68044
Nico Braun <nico-braun@live.de>
2021-04-28 20:12:36 +0000

    add comment

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -3,6 +3,8 @@ from os import walk
 
 
 def register_blueprints(app):
+    """register all blueprints in the blueprints folder.
+    It will look for an object named "blueprint" in those files."""
     for blueprint in next(walk("blueprints"))[1]:
         for name in find_modules(f"blueprints.{blueprint}"):
             mod = import_string(name)
