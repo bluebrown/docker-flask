1a70d117a073db6b5976f2de4bc80e38a03c2c6d
Nico Braun <nico-braun@live.de>
2021-04-30 12:08:33 +0000

    catch generic exception in readiness handler

--- app/blueprints/healthcheck/views/readiness.py
+++ app/blueprints/healthcheck/views/readiness.py
@@ -1,12 +1,16 @@
 from flask.views import View
 from global_objects import db
-from flask import current_app
+from flask import current_app, abort
 
 
 class Ready(View):
     """The Ready class implements the readiness endpoint"""
 
     def dispatch_request(self):
-        info = db.connection.server_info()
-        current_app.logger.debug(info)
-        return ""
+        try:
+            info = db.connection.server_info()
+            current_app.logger.debug(info)
+            return ""
+        except Exception as e:
+            current_app.logger.error(e)
+            abort(503)
